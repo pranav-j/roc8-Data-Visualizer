cfea61206ada3ebb973fce1366cd6ed3d0c7988e
Pranav J <pranavj10@gmail.com>
2024-10-20 16:35:45 +0530

    editted login

--- front-end/src/pages/login/login.jsx
+++ front-end/src/pages/login/login.jsx
@@ -1,72 +1,3 @@
-// import "./../../styles/authForm.css";
-// import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import { loginUser } from '../../redux/slices/userSlice';
-// import Header from "../../components/header";
-
-// function Login() {
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-//     const navigate = useNavigate();
-//     const dispatch = useDispatch();
-
-//     const handleLogin = (event) => {
-//         event.preventDefault();
-
-//         dispatch(loginUser({ email, password }))
-//             .then((response) => {
-//                 if (response.meta.requestStatus === 'fulfilled') {
-//                     navigate('/dashboard');
-//                 } else {
-//                     alert('Login failed. Please check your credentials.');
-//                 }
-//             })
-//             .catch((error) => {
-//                 console.error('Login error:', error);
-//                 alert('Login failed. Please try again.');
-//             });
-//     };
-
-//     return (
-//         <>
-//             <Header />
-//             <div className="auth-form">           
-//                 <h2>Login</h2>
-//                 <form onSubmit={handleLogin}>
-//                     <div className="input-group">
-//                         <label htmlFor="email">Email</label>
-//                         <input
-//                             type="email"
-//                             id="email"
-//                             value={email}
-//                             onChange={(e) => setEmail(e.target.value)}
-//                             required
-//                         />
-//                     </div>
-//                     <div className="input-group">
-//                         <label htmlFor="password">Password</label>
-//                         <input
-//                             type="password"
-//                             id="password"
-//                             value={password}
-//                             onChange={(e) => setPassword(e.target.value)}
-//                             required
-//                         />
-//                     </div>
-//                     <button type="submit">Login</button>
-//                 </form>
-//                 <p onClick={() => navigate('/signup')} className="toggle-link">
-//                     Don't have an account? Sign Up
-//                 </p>
-//             </div>
-//         </>
-//     );
-// }
-
-// export default Login;
-
-
 import "./../../styles/authForm.css";
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
@@ -77,17 +8,17 @@ import Header from "../../components/header";
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [isLoading, setIsLoading] = useState(false); // New state for loading status
+    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const handleLogin = (event) => {
         event.preventDefault();
-        setIsLoading(true); // Set loading to true when login starts
+        setIsLoading(true);
 
         dispatch(loginUser({ email, password }))
             .then((response) => {
-                setIsLoading(false); // Set loading to false after login completes
+                setIsLoading(false);
                 if (response.meta.requestStatus === 'fulfilled') {
                     navigate('/dashboard');
                 } else {
@@ -96,7 +27,7 @@ function Login() {
             })
             .catch((error) => {
                 console.error('Login error:', error);
-                setIsLoading(false); // Set loading to false in case of an error
+                setIsLoading(false); 
                 alert('Login failed. Please try again.');
             });
     };
@@ -115,7 +46,7 @@ function Login() {
                             id="email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            disabled={isLoading} // Disable input while loading
+                            disabled={isLoading}
                             required
                         />
                     </div>
@@ -126,11 +57,11 @@ function Login() {
                             id="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            disabled={isLoading} // Disable input while loading
+                            disabled={isLoading}
                             required
                         />
                     </div>
-                    <button type="submit" disabled={isLoading}> {/* Disable button while loading */}
+                    <button type="submit" disabled={isLoading}>
                         {isLoading ? 'Please wait...' : 'Login'}
                     </button>
                 </form>
