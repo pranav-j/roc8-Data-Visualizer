a091121953d3d469127aeb4770acb8df7fc5cfa8
Pranav J <pranavj10@gmail.com>
2024-10-20 10:28:46 +0530

    added vercel.json

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: 'http://127.0.0.1:5174', credentials: true }));
+// app.use(cors({ origin: 'http://127.0.0.1:5174', credentials: true }));
+
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+
 
 app.use(router);
 
